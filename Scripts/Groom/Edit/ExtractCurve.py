6ee8b8cb385d5d14e9538eed1cf4cafffe8d06af
Jeffreytsai1004 <jeffreytsai1004@gmail.com>
2024-10-16 00:42:56 +0800

    Delete Scripts/Groom/Edit/ExtractCurve.py
    Clean

--- Scripts/Groom/Edit/ExtractCurve.py
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import maya.cmds as cmds
-import maya.mel as mel
-
-def run():
-    mel.eval('polyToCurve -form 2 -degree 3 -conformToSmoothMeshPreview 1')
