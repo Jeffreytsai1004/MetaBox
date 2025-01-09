1ba2ef01e1ebfeba780b7daebda1493ea932d86d
Jeffreytsai1004 <jeffreytsai1004@gmail.com>
2025-01-09 23:30:34 +0800

    Delete Scripts/Metahuman/DNA_Calibration directory

--- Scripts/Metahuman/DNA_Calibration/dna_viewer/dnalib/layer.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Layer(Enum):
-    descriptor = 1
-    definition = 2
-    behavior = 3
-    geometry = 4
-    all = 5
--- Scripts/Metahuman/DNA_Calibration/dnacalib/PyDNA/README.md
+++
@@ -1,7 +0,0 @@
-# PyDNA
-
-Python wrapper for the DNA API.
-
-## Usage
-
-For now, check out the examples under `python3/examples/`.
